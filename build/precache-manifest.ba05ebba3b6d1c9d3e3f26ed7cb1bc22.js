f3fd1e64d86fb14910ec9deab19437352c997ee5
Alexandra Dominique Sanchez <lexie.sanchez@detailonline.tech>
2019-11-20 13:02:13 +0800

    initial build

---
+++ build/precache-manifest.ba05ebba3b6d1c9d3e3f26ed7cb1bc22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36cbb53e3d17061ef0c64398068f82d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e081e48146aa2bb60fa",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "bdcc478d85f249ca5c7c",
+    "url": "/static/js/2.ce8eac4d.chunk.js"
+  },
+  {
+    "revision": "5e081e48146aa2bb60fa",
+    "url": "/static/js/main.19e2a451.chunk.js"
+  },
+  {
+    "revision": "98094525750b4c16090f",
+    "url": "/static/js/runtime-main.eab5aa02.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
